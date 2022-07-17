1f4b472fe878389a81b7152807666238fd5fbdc1
Wenh <850838293@qq.com>
2022-07-17 22:50:23 +0800

    forth commit

--- src/main/java/com/wenh/myapp/controller/UserController.java
+++ src/main/java/com/wenh/myapp/controller/UserController.java
@@ -13,8 +13,6 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     @Autowired
-    private UserMapper userMapper;
-    @Autowired
     private UserService userService;
 
     @GetMapping
